ed93d0cf37c659b247f77d7af732bb0f9863439f
Ivonne Custodio <ivonnecustodio87@gmail.com>
2024-09-04 00:07:57 -0700

    parttwoonly.txt

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-import streamlit as st
+#import streamlit as st
 
-st.header('Tossing a Coin')
+#st.header('Tossing a Coin')
 
 #st.write('It is not a functional application yet. Under construction.')
 
-number_of_trials = st.slider('Number of trials?', 1, 1000, 10)
-start_button = st.button('Run')
+#number_of_trials = st.slider('Number of trials?', 1, 1000, 10)
+#start_button = st.button('Run')
 
-if start_button:
-    st.write(f'Running the experiment of {number_of_trials} trials.')
+#if start_button:
+#    st.write(f'Running the experiment of {number_of_trials} trials.')
 
 #st.write('It is not a functional application yet. Under construction.')
 
